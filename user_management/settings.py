52ad8632655fb1903b692ba948a40b7a609ae69f
soldey <kmsoldatov@gmail.com>
2021-07-04 15:52:49 +0300

    Fix: +password change/reset pages

--- user_management/settings.py
+++ user_management/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'user_management.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / 'users/templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
